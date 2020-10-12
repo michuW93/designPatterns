2ef3a46c161dcd4f4c35eb0cab3a2f4d18fe93eb
michuW93 <michal.walkowiak93@gmail.com>
2020-10-12 21:43:53 +0200

    factory method - refactor

--- src/FactoryMethod/FactoryMethodTest.java
+++ src/FactoryMethod/FactoryMethodTest.java
@@ -3,9 +3,8 @@ package FactoryMethod;
 public class FactoryMethodTest {
     public static void main(String[] args){
         SimplePizzaFactory simplePizzaFactory = new SimplePizzaFactory();
-        simplePizzaFactory.createPizza("Chicago");
 
         PizzaStore pizzaStore = new PizzaStore(simplePizzaFactory);
-        pizzaStore.orderPizza("Chicago");
+        pizzaStore.orderPizza("Boston");
     }
 }
